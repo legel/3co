aed3a72d690b7c7d9873b539b4cecf4579b6c002
Lance Legel <lancelegel@gmail.com>
2019-08-18 22:17:37 -0600

    testing y-only change in focal length for lasers

--- optics.py
+++ optics.py
@@ -247,9 +247,9 @@ class Optics():
         z = np.random.normal(loc=3.0, scale=0.01)
 
       else:  # if we're making a rigid connection to another optical system 
-        x = position_anchor.focal_point.x + np.random.normal(loc=0.0, scale=0.05)
+        x = position_anchor.focal_point.x + np.random.normal(loc=0.0, scale=0.001)
         y = position_anchor.focal_point.y + np.random.normal(loc=0.0, scale=0.05)
-        z = position_anchor.focal_point.z + np.random.normal(loc=0.0, scale=0.01)  
+        z = position_anchor.focal_point.z + np.random.normal(loc=0.0, scale=0.001)  
 
       print("{} focal point of ({},{},{}) sampled".format(self.photonics, x, y, z))
       return Point(x, y, z)
