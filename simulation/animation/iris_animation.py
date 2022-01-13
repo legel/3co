0cc9ee5a9d6341dcf67e03ad85cb1d13a7a961f6
nikesque <nikabuijs@gmail.com>
2022-01-13 09:42:42 +0000

    go

--- simulation/animation/iris_animation.py
+++ simulation/animation/iris_animation.py
@@ -162,44 +162,47 @@ def load_iris():
     return iris
 
     
-def blender_camera_view(frame, x=None,y=None,z=None,pitch=None,yaw=None):
+def blender_camera_view(frame, x=None,y=None,z=None,pitch=None,yaw=None,roll=None):
     # a simple function for controlling the position of rendering camera in Blender!
     # this function should operate with the exact same coordinate system as Iris
     # that is, repeat coordinate system mapping above, so that it becomes easy, e.g., to overlap the Blender renderer and the Iris scanner view
     # LOCAL
     # access the camera in blender 
-    camera = bpy.context.scene.camera # get 'Camera' object from scene
+    # camera = bpy.context.scene.camera # get 'Camera' object from scene
+    camera = bpy.context.scene.objects["Camera"]
+    
+    # HACK: for now just set camera's coordinates by GUI, plugging in those values 
+    # Eventually, would be good if same API could be used, but not vital right now
 
     if type(pitch) != type(None):
+        # iris_pitch = 90 for blender_euler_x = 180
+        # blender_euler_x = pitch + 90 
         # move camera pitch (same as Iris coordinate system)
         camera.rotation_euler[0] = math.radians(pitch) # important to convert to radians for Blender!
 
     if type(yaw) != type(None):
         # move camera yaw
+        #camera.rotation_euler[2] = math.radians(yaw)
         camera.rotation_euler[1] = math.radians(yaw)
 
+    if type(roll) != type(None):
+        camera.rotation_euler[2] = math.radians(roll) # important to convert to radians for Blender!
+
     if type(z) != type(None):
-        # convert Iris coordinate system to Blender coordinate system
-        # iris_z=0.0 maps to blender_y=-1.5, and iris_z=1.5 maps to blender_y=0.0
-        blender_y = (z - 1.5) * 1000 # mm
-
         # move camera z
-        camera.location[1] = blender_y 
+        camera.location[2] = z 
     if type(y) != type(None):
-        # convert Iris coordinate system to Blender coordinate system
-        # iris_y=-1.3 maps to blender_x=1.3, and iris_y=1.2 maps to blender_x=-1.2
-        blender_x = -y * 1000 # mm
-
         # move camera y
-        camera.location[0] = blender_x
+        camera.location[1] = y 
 
     if type(x) != type(None):
-        # convert Iris coordinate system to Blender coordinate system
-        # iris_x=-1.3 maps to blender_z=-1.3, and iris_x=1.2 maps to blender_z=1.2
-        blender_z = x * 1000 # mm
+        # convert Iris coordinate system to Blender camera coordinate system
 
         # move camera x
-        camera.location[2] = blender_z
+        camera.location[0] = x
+
+    camera.keyframe_insert(data_path='location', frame=frame)
+    camera.keyframe_insert(data_path='rotation_euler', frame=frame)
 
 def fps(input_frame_value):
     # change fps based on resolution parameter (good for debugging slow frame rate)
@@ -219,7 +222,7 @@ if __name__ == "__main__":
 
     # camera view is defined in the same coordinate system as the Iris scanner (even if may be outside)
     # in this case, we are setting the camera to be outside of the doors of the scanner, facing inside, through the doors
-    blender_camera_view(frame=fps(0), x=-5.0, y=0.0, z=1.5, pitch=0.0, yaw=-180.0)
+    blender_camera_view(frame=fps(0), x=3.7326*1000, y=10.125*1000, z=1.5858*1000, pitch=90, yaw=0.0, roll=180)
     
     # initialize position of Iris in center, folded up, facing down (a good safe place to start any scan)
     iris.position(frame=fps(0), x=0.0, y=0.0, z=1.5, pitch=-90.0, yaw=0.0)
@@ -258,13 +261,13 @@ if __name__ == "__main__":
     # machine moves down from its starting 1.7 meters up position, to 0.85 meters above the floor
     iris.position(frame=fps(335), z=0.85)
 
-    # now, we would like to move the camera into the same view as Iris
-    # to make sure the camera has been still the entire time for previous actions
-    # we must set again the same camera position at the latest frame
-    blender_camera_view(frame=fps(335), x=-5.0, y=0.0, z=1.7, pitch=0.0, yaw=-180.0)
+    # # now, we would like to move the camera into the same view as Iris
+    # # to make sure the camera has been still the entire time for previous actions
+    # # we must set again the same camera position at the latest frame
+    # blender_camera_view(frame=fps(335), x=-5.0, y=0.0, z=1.7, pitch=0.0, yaw=-180.0)
 
-    # now, let's animation the camera motion, zooming into the perspective of Iris
-    blender_camera_view(frame=fps(395), x=1.5, y=0.0, z=0.85, pitch=0.0, yaw=0.0)
+    # # now, let's animation the camera motion, zooming into the perspective of Iris
+    # blender_camera_view(frame=fps(395), x=1.5, y=0.0, z=0.85, pitch=0.0, yaw=0.0)
 
     # capture another scan, this time a long exposure again; for the first time, we see the scan from Iris's perspective
     iris.scan(frame=fps(395), number_of_frames_to_scan=30)
@@ -272,16 +275,16 @@ if __name__ == "__main__":
     # machine moves back to the far right again, but this time with its z-axis extended half-way, and its head facing forward 
     iris.position(frame=fps(455), y=1.5)
 
-    # as well, as move the camera in synchrony with the machine! "I am become Iris, 3D modeler of worlds."
-    blender_camera_view(frame=fps(455), y=1.5)
+    # # as well, as move the camera in synchrony with the machine! "I am become Iris, 3D modeler of worlds."
+    # blender_camera_view(frame=fps(455), y=1.5)
 
     # machine is in the far right corner, and now orients its head so that it should now be facing to the left
     iris.position(frame=fps(485), yaw=-90)
-    blender_camera_view(frame=fps(485), yaw=-90)
+    # blender_camera_view(frame=fps(485), yaw=-90)
 
     # machine moves along the right side, coming toward us, stopping halfway in the center of x-axis
     iris.position(frame=fps(515), x=0)
-    blender_camera_view(frame=fps(515), x=0)
+    # blender_camera_view(frame=fps(515), x=0)
 
     # capture the last survey scan, one last long exposure
     iris.scan(frame=fps(515), number_of_frames_to_scan=fps(30))
