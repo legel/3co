a4dd6a074d61dd26030adf7777ee4f5b9366e424
Rob <rob@3co.ai>
2022-11-29 12:00:55 -0700

    refactored and updated sdf_to_mesh for merging functionality

--- nerf/sdf_to_mesh.py
+++ nerf/sdf_to_mesh.py
@@ -2,43 +2,75 @@ import torch
 import numpy as np
 import mcubes
 import open3d as o3d
+import sys
 
-#filename = 'data/dragon_scale/hyperparam_experiments/pretrained_with_entropy_loss_200k/sdf/sdf_120meshes_vox001.npy'
-filename = 'sdf_with2.npy'
+def sdf2mesh(in_file_name, out_file_name):
+    print('Meshing', in_file_name)
+    sdf_in = torch.from_numpy(np.load(in_file_name))
 
-sdf_in = torch.from_numpy(np.load(filename))
+    sdf_indices = torch.argwhere(sdf_in == sdf_in)
+    sdf = np.transpose(np.asarray([
+        sdf_indices[:,0].numpy(), 
+        sdf_indices[:,1].numpy(), 
+        sdf_indices[:,2].numpy(), 
+        sdf_in[sdf_indices[:,0].numpy(), sdf_indices[:,1].numpy(), sdf_indices[:,2].numpy()].numpy()
+    ]))                      
 
-sdf_indices = torch.argwhere(sdf_in == sdf_in)
-sdf = np.transpose(np.asarray([
-    sdf_indices[:,0].numpy(), 
-    sdf_indices[:,1].numpy(), 
-    sdf_indices[:,2].numpy(), 
-    sdf_in[sdf_indices[:,0].numpy(), sdf_indices[:,1].numpy(), sdf_indices[:,2].numpy()].numpy()
-]))                      
+    sdf = torch.tensor(sdf)
 
-sdf = torch.tensor(sdf)
+    #sdf = torch.from_numpy(np.load(filename))
 
-#sdf = torch.from_numpy(np.load(filename))
+    x = sdf[:,0].long()
+    y = sdf[:,1].long()
+    z = sdf[:,2].long()
+    d = sdf[:,3].float()
 
-x = sdf[:,0].long()
-y = sdf[:,1].long()
-z = sdf[:,2].long()
-d = sdf[:,3].float()
+    max_x_i = torch.max(x,dim=0)[0]
+    max_y_i = torch.max(y,dim=0)[0]
+    max_z_i = torch.max(z,dim=0)[0]
 
-max_x_i = torch.max(x,dim=0)[0]
-max_y_i = torch.max(y,dim=0)[0]
-max_z_i = torch.max(z,dim=0)[0]
+    u = torch.zeros(max_x_i+1, max_y_i+1, max_z_i+1)
 
-u = torch.zeros(max_x_i+1, max_y_i+1, max_z_i+1)
+    u[ sdf[:,0].long(), sdf[:,1].long(), sdf[:,2].long() ] = sdf[:,3].float()    
 
+    u = u.numpy()
 
-u[ sdf[:,0].long(), sdf[:,1].long(), sdf[:,2].long() ] = sdf[:,3].float()
-print(u.size())
+    # do we match marching cube's voxel representation?
+    vertices, triangles = mcubes.marching_cubes(u,0.0015)
+    print("vertices, triangles:", vertices.shape, triangles.shape)
 
-u = u.numpy()
+    print('Outputting', out_file_name)
+    mcubes.export_mesh(vertices, triangles, out_file_name, "NiceMesh")
 
-# do we watch marching cube's voxel representation?
-vertices, triangles = mcubes.marching_cubes(u,0.01)
-print("vertices, triangles:", vertices.shape, triangles.shape)
+def merge_sdfs(in_file_names, out_file_name):
 
-mcubes.export_mesh(vertices, triangles, "mesh.dae", "NiceMesh")
+    sdf_merged = None
+    for sdf_i in [torch.from_numpy(np.load(f_name)) for f_name in in_file_names]:
+        #sdf_i = torch.tensor(sdf_i.clone().detach())
+        if sdf_merged == None:
+            sdf_merged = sdf_i
+        else:
+            sdf_merged = torch.min( torch.cat ( [sdf_merged.unsqueeze(1), sdf_i.unsqueeze(1)], dim=1 ), dim=1 )[0]            
+    
+    with open(out_file_name, "wb") as f:
+        np.save(f, sdf_merged.numpy())    
+    
+
+if __name__ == '__main__':
+
+    with torch.no_grad():
+        args = sys.argv
+
+        if args[1] == 'sdf2mesh':
+            in_file_name = 'sdfs/sdf_merged.npy'
+            out_file_name = 'mesh_merged.dae'
+            #in_file_name = 'sdfs/vox001/sdf.npy'
+            #out_file_name = 'vox001_mesh.dae'            
+            sdf2mesh(in_file_name, out_file_name)
+        elif args[1] == 'merge':
+            n_sdfs = 4
+            in_file_names = ['sdfs/sdf_{}_real.npy'.format(i) for i in range(n_sdfs)]
+            out_file_name = 'sdfs/sdf_merged.npy'
+            merge_sdfs(in_file_names, out_file_name)
+        else:
+            print("Usage: sdf2mesh or merge")
