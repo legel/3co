00bcb4be83816696a27e4930abb94d407d364ac3
Rob <rob@3co.ai>
2022-11-28 18:39:36 -0700

    VCG version 2

--- nerf/sdf_to_mesh.py
+++ nerf/sdf_to_mesh.py
@@ -3,6 +3,7 @@ import numpy as np
 import mcubes
 import open3d as o3d
 
+#filename = 'data/dragon_scale/hyperparam_experiments/pretrained_with_entropy_loss_200k/sdf/sdf_120meshes_vox001.npy'
 filename = 'sdf_120meshes_vox001.npy'
 
 sdf = torch.from_numpy(np.load(filename))
@@ -18,11 +19,14 @@ max_z_i = torch.max(z,dim=0)[0]
 
 u = torch.zeros(max_x_i+1, max_y_i+1, max_z_i+1)
 
+
 u[ sdf[:,0].long(), sdf[:,1].long(), sdf[:,2].long() ] = sdf[:,3].float()
-   
+print(u.size())
+
 u = u.numpy()
 
-vertices, triangles = mcubes.marching_cubes(u,0.001)
+# do we watch marching cube's voxel representation?
+vertices, triangles = mcubes.marching_cubes(u,0.005)
 print("vertices, triangles:", vertices.shape, triangles.shape)
 
 mcubes.export_mesh(vertices, triangles, "mesh.dae", "NiceMesh")
--- nerf/surface_reconstruction.py
+++ nerf/surface_reconstruction.py
@@ -70,20 +70,127 @@ def construct_volume(vox_size):
     volume = torch.cat([grid_x.unsqueeze(3), grid_y.unsqueeze(3), grid_z.unsqueeze(3)], dim=3)
     return volume
 
-def distance_to_mesh(p, face_vertices, dists):
+def triangle_normal(tri):      
             
+    qr = tri[...,1] - tri[...,0]
+    qs = tri[...,1] - tri[...,0]    
+    n = torch.cross(qr, qs, dim=1)
+    return torch.nn.functional.normalize(n, p=2, dim=1)    
+
+
+# Determine whether point p is inside triangle tri. 
+# This function assumes that p lies in the same plane as tri.
+# p_expand: [number_of_faces, number_of_voxels, 3]
+# faces_expand: [number_of_faces, number_of_voxels, 3]
+def is_in_triangle(p_expand, faces_expand): #, ray_origin_to_plane_distance)
+
+
+  theta1 = vectors_angle(p_expand - faces_expand[:,:, 0], p_expand - faces_expand[:,:, 1])
+  theta2 = vectors_angle(p_expand - faces_expand[:,:, 0], p_expand - faces_expand[:,:, 2])
+  theta3 = vectors_angle(p_expand - faces_expand[:,:, 1], p_expand - faces_expand[:,:, 2])      
+
+  return torch.abs(theta1 + theta2 + theta3 - 2.0*math.pi) < 0.001
+
+  """
+  return torch.logical_and(
+    torch.abs(theta1 + theta2 + theta3 - 2.0*math.pi) < 0.001,
+    ray_origin_to_plane_distance > 0 # check if in correct direction
+  )  
+  """
+
+# compute the angle between two vectors, in radians
+def vectors_angle(a, b):
+
+  n = torch.norm(a,dim=2) * torch.norm(b,dim=2)
+  return torch.acos(torch.sum(a*b,dim=2) / n )    
+
+# https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
+# https://github.com/facebookresearch/pytorch3d/blob/main/pytorch3d/csrc/utils/geometry_utils.cuh
+# edges: [number_of_faces_in_view * 3, 2, 3]
+def min_distances_to_edges(p, edges):
+
+  # need to check if points of edge / p are the same?    
+  v0 = edges[:, 0]
+  v1 = edges[:, 1]  
+  
+  v1v0 = v1 - v0
+  p_expand = p.unsqueeze(1).expand(p.size()[0], edges.size()[0], 3)
+  v0_expand = v0.unsqueeze(0).expand(p.size()[0], edges.size()[0], 3)
+  
+  # pv0: [p, number_of_faces_in_view * 3, 3]  
+  pv0 = p_expand - v0_expand
+
+  v1v0_expand = v1v0.unsqueeze(0).expand(p.size()[0], edges.size()[0], 3)
+  t_bot = torch.sum(v1v0*v1v0,dim=1) # squared distance between v0 and v1         
+  t_top = torch.sum(v1v0*pv0,dim=2)
+  
+  t_bot_expand = t_bot.unsqueeze(0).expand(p.size()[0], edges.size()[0])
+  t_top_expand = t_top
+  tt = t_top_expand / t_bot_expand  
+  tt_expand = tt.unsqueeze(2).expand(p.size()[0], edges.size()[0], 3)
+
+  p_proj = v0_expand + tt_expand * v1v0_expand
+  
+  # if tt < 0, closest point is v0
+  # if tt > 1, closest point is v1
+  # otherwise, closest point lies between v0 and v1
+  closest_to_v0_indices = torch.argwhere(tt < 0)
+  closest_to_v1_indices = torch.argwhere(tt > 1)
+  closest_between_v0v1_indices = torch.argwhere(torch.logical_and(tt >= 0, tt <= 1))
+
+  dists_to_edges = torch.zeros(p.size()[0], edges.size()[0]).to(torch.device('cuda:0')) + float('Inf') # 404: fear not found
+  dists_to_edges[closest_to_v0_indices[:,0], closest_to_v0_indices[:,1]] = torch.sqrt(torch.sum( (v0[closest_to_v0_indices[:,1]] - p[closest_to_v0_indices[:,0]])**2, dim=1))
+  dists_to_edges[closest_to_v1_indices[:,0], closest_to_v1_indices[:,1]] = torch.sqrt(torch.sum( (v1[closest_to_v1_indices[:,1]] - p[closest_to_v1_indices[:,0]])**2, dim=1))
+  dists_to_edges[closest_between_v0v1_indices[:,0], closest_between_v0v1_indices[:,1]] =  torch.sqrt(
+      torch.sum(
+        (p_proj[closest_between_v0v1_indices[:,0], closest_between_v0v1_indices[:,1]] - p[closest_between_v0v1_indices[:,0]])**2, dim=1  
+      )
+    )
+
+  min_dists = torch.min(dists_to_edges, dim=1)[0]
+    
+  return min_dists
+
+
+# p: [number_of_voxels_in_batch, 3]
+# face_vertices: [number_of_faces_in_view, 3, 3]
+# dists: [number_of_voxels_in_batch]
+def distance_to_mesh_V2(p, face_vertices, dists):
+    
     p_expand = p.unsqueeze(0).expand(face_vertices.size()[0], p.size()[0], 3)
+    face_vertices_expand = face_vertices.unsqueeze(1).expand(p_expand.size()[0], p_expand.size()[1], 3,3)
 
-    # compute distance from each voxel to closest face of mesh
-    face_centers = torch.mean(face_vertices, dim=1)
-    face_centers_expand = face_centers.unsqueeze(1).expand(p_expand.size()[0], p_expand.size()[1], 3)
-    distance_to_face_centers = torch.sqrt( torch.sum((face_centers_expand-p_expand)**2,dim=2) )        
-    min_distance_to_face_centers = torch.min(distance_to_face_centers, dim=0)[0]
+    tri_normal = triangle_normal(face_vertices)
+    tri_normal_expand = tri_normal.unsqueeze(1).expand(face_vertices.size()[0], p.size()[0], 3)
 
-    return torch.min( 
-      torch.cat([min_distance_to_face_centers.unsqueeze(1), 
-      dists.unsqueeze(1)], dim=1), dim=1
-    )[0]        
+    origin_to_plane_distance = -torch.sum(tri_normal * face_vertices[:,0],dim=1)
+    
+    n_dot_direction = torch.sum(tri_normal * tri_normal, dim=1)
+    n_dot_direction_expand = n_dot_direction.unsqueeze(1).expand(face_vertices.size()[0], p.size()[0])
+    origin_to_plane_distance_expand = origin_to_plane_distance.unsqueeze(1).expand(tri_normal.size()[0], p.size()[0])
+    n_dot_p = torch.sum(tri_normal_expand*p_expand,dim=2)    
+
+    ray_origin_to_plane_distance = -(n_dot_p + origin_to_plane_distance_expand) / n_dot_direction_expand
+    p_hit = p_expand + tri_normal_expand * torch.abs(ray_origin_to_plane_distance).unsqueeze(2)
+    
+    # update sdf for the voxels that project inside one or more triangles    
+    update_indices = torch.argwhere(is_in_triangle(p_hit, face_vertices_expand))
+    update_ray_origin_to_plane_distance = ray_origin_to_plane_distance[update_indices[:,0], update_indices[:,1]]
+    dists[update_indices[:,1]] = torch.min(torch.cat([dists[update_indices[:,1]].unsqueeze(1), update_ray_origin_to_plane_distance.unsqueeze(1)], dim=1), dim=1)[0]                
+    
+    # update sdf for the rest of the voxels, where the closest point must be on an edge
+
+    # edges: [number_of_faces_in_view, 3, 2, 3]
+    edges = torch.stack([face_vertices[:,[0,1]], face_vertices[:,[1,2]], face_vertices[:,[0,2]]], dim=1)
+    # we just need the edges, no need to index by triangle: [number_of_faces_in_view * 3, 2, 3]
+    edges = edges.reshape(face_vertices.size()[0]*3, 2, 3)
+
+    min_edge_dists = min_distances_to_edges(p, edges)
+
+    dists = torch.min( torch.cat ( [dists.unsqueeze(1), min_edge_dists.unsqueeze(1)], dim=1 ), dim=1 )[0]
+    
+    return dists
+                    
         
 def vcg(meshes, poses, pixel_directions_world, out_f_name):
     
@@ -92,7 +199,7 @@ def vcg(meshes, poses, pixel_directions_world, out_f_name):
     W = 640    
     near = 0.1
     far = 1.0        
-    vox_size = 0.01
+    vox_size = 0.005
     save_sdf_frequency = 1
     all_vertices = [mesh.vertices for mesh in meshes]
     set_bounds(all_vertices, vox_size)
@@ -148,10 +255,12 @@ def vcg(meshes, poses, pixel_directions_world, out_f_name):
         view_volume = volume[viewable_voxel_indices[:,0], viewable_voxel_indices[:,1], viewable_voxel_indices[:,2]]                
 
         # Unpack numpy data from open3D and convert to tensor
-        faces = np.asarray(meshes[cam_index].triangles)
-        vertices = np.asarray(meshes[cam_index].vertices)
+        # TODO: currently we're duplicating vertices between faces
+        faces = np.asarray(meshes[cam_index].triangles)        
+        vertices = np.asarray(meshes[cam_index].vertices)        
         face_vertices = torch.tensor(vertices[faces]).float().to(torch.device('cuda:0'))          
-        print("\nmesh {} out of {} ({} faces)".format(cam_index,  poses.size()[0], faces.size))
+                
+        print("\nmesh {} out of {} ({} faces)".format(cam_index,  poses.size()[0], faces.shape[0] ))
                                 
         # Split data into batches based on estimate of GPU memory requirements per batch
         total_bytes_needed = (
@@ -159,13 +268,14 @@ def vcg(meshes, poses, pixel_directions_world, out_f_name):
           view_volume.size()[0] * 3 +
           face_vertices.size()[0] * 3
         ) * 8
-        total_bytes_needed *= 2 # overestimate by a bit
+        total_bytes_needed *= 12 # overestimate by a bit
              
         bytes_per_batch = 4000000000 # 4gb
         n_batches = total_bytes_needed // bytes_per_batch + 1
         batch_size = view_volume.size()[0] // n_batches
                         
         print('batch size: {}'.format(batch_size))
+        print('total voxels for this view:', view_volume.size()[0])        
                 
         view_volume_batches = view_volume.split(batch_size)
         viewable_voxel_indices_batches = viewable_voxel_indices.split(batch_size)
@@ -174,7 +284,7 @@ def vcg(meshes, poses, pixel_directions_world, out_f_name):
         for view_volume_batch, viewable_voxel_indices_batch in zip(view_volume_batches, viewable_voxel_indices_batches):          
           
           dists = sdf[viewable_voxel_indices_batch[:,0], viewable_voxel_indices_batch[:,1], viewable_voxel_indices_batch[:,2]]
-          dists = distance_to_mesh(view_volume_batch, face_vertices, dists)          
+          dists = distance_to_mesh_V2(view_volume_batch, face_vertices, dists)          
           sdf[viewable_voxel_indices_batch[:,0], viewable_voxel_indices_batch[:,1], viewable_voxel_indices_batch[:,2]] = dists
           batch_number += 1
 
@@ -191,8 +301,7 @@ def vcg(meshes, poses, pixel_directions_world, out_f_name):
                 valid_sdf_indices[:,1].numpy(), 
                 valid_sdf_indices[:,2].numpy(), 
                 sdf[valid_sdf_indices[:,0].numpy(), valid_sdf_indices[:,1].numpy(), valid_sdf_indices[:,2].numpy()].numpy()
-              ]))              
-              print(result.shape)
+              ]))                            
               np.save(f, result)
 
             sdf = sdf.to(torch.device('cuda:0'))
@@ -237,8 +346,3 @@ if __name__ == '__main__':
 
         out_f_name = 'sdf_120meshes_vox001.npy'        
         sdf = vcg(meshes, poses, pixel_world_directions, out_f_name)
-
-
-
-
-    
