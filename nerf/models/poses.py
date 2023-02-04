fix to apple quaternion -> rotation matrix in pytorch3d coordinate system -> back to apple coordinate system. Pixel directions now match apple coordinate system

nerf/models/poses.py
@@ -15,8 +15,18 @@ class CameraPoseModel(nn.Module):
 
     def forward(self, i=None):
         r = self.r  # (N, 3) axis-angle
-        t = self.t  # (N, 3)
-        delta_pose = make_pose(r, t)  # (N, 4, 4)
+        t = self.t  # (N, 3)        
 
+        delta_pose = make_pose(r, t)  # (N, 4, 4)
         poses = delta_pose @ self.poses   
+        
+        #rotation_matrix[:, 2] = -rotation_matrix[:, 2]
+        #rotation_matrix[2, :3] = rotation_matrix[2, :3]
+        #translation_vector[2] = -translation_vector[2] 
+        #poses[:, :, 2] = -poses[:, :, 2]
+        #poses[:, 2, :3] = poses[:, 2]       
         return poses
+
+        #c2w = make_c2w(r, t)
+        #c2w = c2w @ self.poses[i]
+        #return c2w        

nerf/utils/volume_op.py
@@ -11,7 +11,8 @@ def volume_sampling(poses, pixel_directions, sampling_depths, perturb_depths=Tru
     N_samples = sampling_depths.shape[1]
 
     # transform rays from camera coordinate to world coordinate    
-    pixel_directions_world = torch.matmul(poses[:,:3, :3], pixel_directions.unsqueeze(2)).squeeze(2)  # (N, 3, 3) * (N, 3, 1) -> (N, 3) .squeeze(3) 
+    pixel_directions_world = torch.matmul(poses[:,:3, :3], pixel_directions.unsqueeze(2)).squeeze(2)  # (N, 3, 3) * (N, 3, 1) -> (N, 3) .squeeze(3)     
+    
     poses_xyz = poses[:, :3, 3]  # the translation vectors (N, 3)
 
     # this perturb only works if we sample depth linearly, not the disparity.