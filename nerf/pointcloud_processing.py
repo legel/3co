256fae87babdf1b219d61a725cbdf77b379750f2
Rob <rob@3co.ai>
2023-04-23 09:02:09 -0600

    update pointcloud_processing for new training image format

--- nerf/pointcloud_processing.py
+++ nerf/pointcloud_processing.py
@@ -30,27 +30,27 @@ os.environ['PYTORCH_KERNEL_CACHE_PATH'] = './'
 def generate_point_clouds():
 
     dynamic_args = {
-        "base_directory" : '\'./data/dragon_scale_large\'',
-        "number_of_samples_outward_per_raycast" : 360,
-        "number_of_samples_outward_per_raycast_for_test_renders" : 360,
+        "base_directory" : '\'./data/orchid\'',
+        "number_of_samples_outward_per_raycast" : 720,
+        "number_of_samples_outward_per_raycast_for_test_renders" : 720,
         "density_neural_network_parameters" : 256,
         "percentile_of_samples_in_near_region" : 0.8,
         "number_of_pixels_per_batch_for_test_renders" : 1000,   
-        "pixel_samples_per_epoch" : 500,         
         #"H_for_test_renders" : 1440,
         #"W_for_test_renders" : 1920,
-        "H_for_test_renders" : 480,
-        "W_for_test_renders" : 640,            
+        "H_for_test_renders" : 1440,
+        "W_for_test_renders" : 1920,            
         "near_maximum_depth" : 0.5,
-        "skip_every_n_images_for_training" : 60,
+        "number_of_images_in_training_dataset" : 120,
+        "number_of_test_images" : 120,
+        "number_of_pixels_in_training_dataset" : 640 * 480 * 256,
         "skip_every_n_images_for_testing" : 1,
-        "use_sparse_fine_rendering" : False,
-        "pretrained_models_directory" : '\'./data/dragon_scale/hyperparam_experiments/from_cloud/dragon_scale_run39/models\'',        
+        "use_sparse_fine_rendering" : True,
+        "pretrained_models_directory" : '\'./data/orchid/hyperparam_experiments/from_cloud/orchid_run204/models\'',        
         #"pretrained_models_directory" : '\'./data/cactus/hyperparam_experiments/from_cloud/cactus_run43/models\'',        
         "start_epoch" : 500001,
         "load_pretrained_models" : True,            
-        "number_of_epochs" : 1,    
-        "number_of_test_images" : 500,        
+        "number_of_epochs" : 1,            
     }  
     with torch.no_grad():
         scene = SceneModel(args=parse_args(), experiment_args='dynamic', dynamic_args=dynamic_args)
@@ -67,18 +67,10 @@ def generate_point_clouds():
     W = scene.args.W_for_test_renders        
     
     all_focal_lengths = scene.models["focal"]()([0])
+    
+    for image_index in scene.test_image_indices:
 
-    test_image_indices = scene.test_image_indices
-    sub_test_image_indices = test_image_indices
-    # sub_test_image_indices = [0,159,222]        
-    for image_index in [scene.test_image_indices[i] for i in sub_test_image_indices]:        
-                                
-        pixel_indices = torch.argwhere(scene.image_ids_per_pixel == image_index)                                    
-        ground_truth_rgb_img, image_name = scene.load_image_data(image_index * scene.args.skip_every_n_images_for_training)                        
-        #sensor_depth, near_bound, far_bound, confidence = scene.load_depth_data(image_index) # (H, W)                        
-                                        
-        pixel_rows = scene.pixel_rows[pixel_indices]
-        pixel_cols = scene.pixel_cols[pixel_indices]        
+        ground_truth_rgb_img, _ = scene.load_image_data(image_index * scene.skip_every_n_images_for_training)
 
         pp_x = scene.principal_point_x * (W / scene.W)
         pp_y = scene.principal_point_y * (H / scene.H)           
@@ -95,31 +87,26 @@ def generate_point_clouds():
             pp_y
         )
 
-        poses_for_this_img = scene.models['pose'](0)[image_index].unsqueeze(0).expand(W*H, -1, -1)                            
-        render_result = scene.render_prediction(poses_for_this_img, focal_lengths_for_this_img, H, W, pp_x, pp_y)            
+        poses_for_this_img = scene.models['pose']()([0])[image_index].unsqueeze(0).expand(W*H, -1, -1)                            
+        render_result = scene.render_prediction(poses_for_this_img, focal_lengths_for_this_img, pp_x, pp_y)            
         depth_fine = render_result['rendered_depth_fine']            
                             
         # save rendered rgb and depth images
         out_file_suffix = str(image_index)
         color_file_name_fine = os.path.join(scene.color_out_dir, str(out_file_suffix).zfill(4) + '_color_fine_{}.png'.format(epoch))
-        depth_file_name_fine = os.path.join(scene.depth_out_dir, str(out_file_suffix).zfill(4) + '_depth_fine_{}.png'.format(epoch))                    
-        normals_file_name = os.path.join(scene.normals_out_dir, str(out_file_suffix).zfill(4) + '_normals_{}.png'.format(epoch))            
+        depth_file_name_fine = os.path.join(scene.depth_out_dir, str(out_file_suffix).zfill(4) + '_depth_fine_{}.png'.format(epoch))                            
         if scene.args.use_sparse_fine_rendering:
             color_file_name_fine = os.path.join(scene.color_out_dir, str(out_file_suffix).zfill(4) + 'sparse_color_fine_{}.png'.format(epoch))
             depth_file_name_fine = os.path.join(scene.depth_out_dir, str(out_file_suffix).zfill(4) + 'sparse_depth_fine_{}.png'.format(epoch))                            
         
-        scene.save_render_as_png(render_result, H, W, color_file_name_fine, depth_file_name_fine, None, None, normals_file_name)
+        scene.save_render_as_png(render_result, H, W, color_file_name_fine, depth_file_name_fine, None, None)
     
-        print("Saving .ply for view {}".format(image_index))                
-        pointcloud_file_name = os.path.join(scene.pointcloud_out_dir, str(out_file_suffix).zfill(4) + '_depth_view_{}.png'.format(epoch))
+        print("Saving .ply for view {}".format(image_index))                        
         pose = scene.models['pose']()([0])[image_index].cpu()
         unsparse_rendered_rgb_img = None
         
         rendered_depth_img = render_result['rendered_depth_fine'].reshape(H, W).to(device=scene.device)
         rendered_rgb_img = render_result['rendered_image_fine'].reshape(H, W, 3).to(device=scene.device)
-        #normals = render_result['rendered_normals'].reshape(H, W, 3).to(device=scene.device)
-        #normals_img = normals.reshape(H, W, 3)
-
         
         if scene.args.use_sparse_fine_rendering:
             unsparse_rendered_rgb_img = render_result['rendered_image_unsparse_fine'].reshape(H, W, 3).cpu()
@@ -128,13 +115,14 @@ def generate_point_clouds():
             depth_file_name_unsparse_fine = os.path.join(scene.depth_out_dir, str(out_file_suffix).zfill(4) + '_depth_fine_{}.png'.format(epoch))                                    
             render_result['rendered_image_fine'] = unsparse_rendered_rgb_img
             render_result['rendered_depth_fine'] = unsparse_depth_img
-            scene.save_render_as_png(render_result, H, W, color_file_name_unsparse_fine, depth_file_name_unsparse_fine, None, None, None)
+            scene.save_render_as_png(render_result, H, W, color_file_name_unsparse_fine, depth_file_name_unsparse_fine, None, None)
         else:
             unsparse_rendered_rgb_img = None
             unsparse_depth_img = None                
 
-        entropy_image = torch.zeros(H * W, 1)                                
-        entropy_coarse = render_result['entropy_coarse']            
+        depth_weights = render_result['depth_weights_coarse']
+        entropy_coarse = -1 * torch.sum( (depth_weights+scene.args.epsilon) * torch.log2(depth_weights + scene.args.epsilon), dim=1)
+        entropy_image = torch.zeros(H * W, 1)                                        
         entropy_image = entropy_coarse.reshape(H, W)                              
         
         # resize ground truth rgb to match output resolution
@@ -161,11 +149,11 @@ def generate_point_clouds():
             unsparse_depth=unsparse_depth_img,
             max_depth_filter_image=None,
             use_sparse_fine_rendering=scene.args.use_sparse_fine_rendering,
-            normals=None
         )
 
-        label="_{}_{}".format(epoch,image_index)
+        label="_{}_{}".format(epoch,image_index)        
         file_name = "{}/pointclouds/view_{}.ply".format(scene.experiment_dir, label)
+        print(file_name)
         o3d.io.write_point_cloud(file_name, pcd, write_ascii = True)        
 
 
@@ -202,7 +190,6 @@ def generate_point_cloud(
     unsparse_depth=None, 
     max_depth_filter_image=None,    
     use_sparse_fine_rendering=True,
-    normals=None
 ):        
     
     pixel_directions = pixel_directions.reshape(H, W, 3)        
@@ -250,20 +237,14 @@ def generate_point_cloud(
     depth = depth[joint_condition_indices]
     gt_rgb = gt_rgb[joint_condition_indices]
 
-    if normals is None:
-        normals = torch.zeros(H, W, 3)
-        normals = pose[:3, 3] - xyz_coordinates
-        normals = torch.nn.functional.normalize(normals, p=2, dim=2)        
-
-    #normals = normals[joint_condition_indices]
     xyz_coordinates = xyz_coordinates[joint_condition_indices]
     
-    pcd = create_point_cloud(xyz_coordinates, gt_rgb, normals, flatten_xyz=False, flatten_image=False)
+    pcd = create_point_cloud(xyz_coordinates, gt_rgb, flatten_xyz=False, flatten_image=False)
 
     return pcd             
 
 
-def create_point_cloud(xyz_coordinates, colors, normals=None, flatten_xyz=True, flatten_image=True):
+def create_point_cloud(xyz_coordinates, colors, flatten_xyz=True, flatten_image=True):
     pcd = o3d.geometry.PointCloud()
     if flatten_xyz:
         xyz_coordinates = torch.flatten(xyz_coordinates, start_dim=0, end_dim=1).cpu().detach().numpy()
@@ -271,21 +252,11 @@ def create_point_cloud(xyz_coordinates, colors, normals=None, flatten_xyz=True, 
         xyz_coordinates = xyz_coordinates.cpu().detach().numpy()
     if flatten_image:
         colors = torch.flatten(colors, start_dim=0, end_dim=1).cpu().detach().numpy()
-        if normals is not None:
-            normals = torch.flatten(normals, start_dim=0, end_dim=1).cpu().detach().numpy()
     else:
         colors = colors.cpu().detach().numpy()
-        if normals is not None:
-            normals = normals.cpu().detach().numpy()
 
     pcd.points = o3d.utility.Vector3dVector(xyz_coordinates)
-
-    # force open3d to include normals data structure
-    pcd.estimate_normals()
-    
     pcd.colors = o3d.utility.Vector3dVector(colors)
-    if normals is not None:
-        pcd.normals = o3d.utility.Vector3dVector(normals)
 
     return pcd
     
@@ -316,124 +287,53 @@ def merge_and_filter_by_center_radius(directory):
     print('avg_xyz: ', avg_xyz)
 
     merged_points = []
-    merged_normals = []
     merged_colors = []
 
     for file_name in file_names:        
         if file_name.split('.')[-1] == 'ply':
             pc = o3d.io.read_point_cloud(directory + "/{}".format(file_name.split('/')[-1]))
             points = torch.tensor(np.asarray(pc.points))
-            normals = torch.tensor(np.asarray(pc.normals))
             colors = torch.tensor(np.asarray(pc.colors))
             filter = torch.argwhere(torch.sqrt(torch.sum((points - avg_xyz)**2, dim=1)) < filter_radius).squeeze(1)
                                     
             filtered_points = points[filter]
-            filtered_normals = normals[filter]
             filtered_colors = colors[filter]            
 
             merged_points.append(filtered_points)
-            merged_normals.append(filtered_normals)
             merged_colors.append(filtered_colors)
 
-    merged_points = torch.cat(merged_points, dim=0).cpu().detach().numpy()
-    merged_normals = torch.cat(merged_normals, dim=0).cpu().detach().numpy()
+    merged_points = torch.cat(merged_points, dim=0).cpu().detach().numpy()    
     merged_colors = torch.cat(merged_colors, dim=0).cpu().detach().numpy()
         
-    pc.points = o3d.utility.Vector3dVector(merged_points)
-    pc.normals = o3d.utility.Vector3dVector(merged_normals)
+    pc.points = o3d.utility.Vector3dVector(merged_points)    
     pc.colors = o3d.utility.Vector3dVector(merged_colors)
 
     cl, idx = pc.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)        
-    pc.points = o3d.utility.Vector3dVector(merged_points[idx])
-    pc.normals = o3d.utility.Vector3dVector(merged_normals[idx])
+    pc.points = o3d.utility.Vector3dVector(merged_points[idx])    
     pc.colors = o3d.utility.Vector3dVector(merged_colors[idx])    
 
     f_out_name = '{}/{}'.format(directory, 'filtered_and_merged.ply')
     return pc
-
-"""
-    def remove_plane(pointcloud, distance_threshold):
-
-        distance_threshold = torch.tensor([distance_threshold]).to(device)
-        #pc = o3d.io.read_point_cloud(pointcloud_path)
-        pc = pointcloud
-        points = torch.tensor(np.asarray(pc.points), requires_grad=False).float().to(device)
-
-        # represent plane as ax + by + cz + d    
-        center_xyz = torch.sum(points, dim=0) / points.size()[0]    
-        min_y = torch.min(points[:,1])
-
-        # initialize plane as flat and near center of pointcloud
-        plane = torch.zeros(6, dtype=torch.float32, device=device)
-        plane[:3] = torch.tensor([0.0, 1.0, 0.0])
-        plane[3:] = torch.tensor([center_xyz[0], min_y, center_xyz[2]])
-
-        plane = plane.requires_grad_()
-        
-        opt = torch.optim.Adam([plane], lr=0.01)
-        max_epochs = 10000
-        min_epochs = 100
-        stop_condition = 0.0000001
-        converged = False
-        loss_history = np.array([])
-        i = 0
-        while i < max_epochs and converged == False:    
-            opt.zero_grad()
-            loss = torch.sqrt(torch.sum(pc_to_plane_distances(points, plane), dim=0) / points.size()[0])        
-            loss.backward()
-            opt.step()
-            loss_history = np.append(loss_history, loss.cpu().detach().numpy())
-            print(plane)
-            print(loss)
-
-            if i > min_epochs and (loss_history[-100:].sum()/100.0 - loss_history[-30:].sum()/30.0) < stop_condition:
-                converged = True        
-            i=i+1            
-
-        print('final loss: ', loss)
-        print('plane: ({:.3f}, {:.3f}, {:.3f}) ; ({:.3f}, {:.3f}, {:.3f})'.format(plane[0],plane[1],plane[2],plane[3],plane[4],plane[5])),     
-
-        distances = pc_to_plane_distances(points, plane)    
-
-        normals = torch.tensor(np.asarray(pc.normals)).to(device)
-        colors = torch.tensor(np.asarray(pc.colors)).to(device)
-        filter = torch.argwhere(distances > distance_threshold).squeeze(1)
-        print('filtered {}/{} points '.format(points.size()[0] - filter.size()[0], points.size()[0]))
-
-        filtered_points = points[filter].cpu().detach().numpy()
-        filtered_normals = normals[filter].cpu().detach().numpy()
-        filtered_colors = colors[filter].cpu().detach().numpy()       
-            
-        pc.points = o3d.utility.Vector3dVector(filtered_points)
-        pc.normals = o3d.utility.Vector3dVector(filtered_normals)
-        pc.colors = o3d.utility.Vector3dVector(filtered_colors)
-
-        return pc
-"""        
 
 
 def remove_plane(pointcloud, threshold=0.005, init_n=3, iterations=1000):
 
     w, idx = pointcloud.segment_plane(threshold, init_n, iterations)
 
-    points = np.asarray(pointcloud.points)
-    normals = np.asarray(pointcloud.normals)
+    points = np.asarray(pointcloud.points)    
     colors = np.asarray(pointcloud.colors)    
     
     points_below_plane = points[:, 1] <= w[3]    
 
     points = np.delete(points, idx,0)    
-    normals = np.delete(normals, idx,0)
     colors = np.delete(colors, idx,0)
     
     points_below_plane = np.squeeze(np.argwhere( points[:, 1] <= -w[3] + 0.025), axis=1)        
 
-    points = np.delete(points, points_below_plane,0)    
-    normals = np.delete(normals, points_below_plane,0)
+    points = np.delete(points, points_below_plane,0)        
     colors = np.delete(colors, points_below_plane,0)    
 
-    pointcloud.points = o3d.utility.Vector3dVector(np.squeeze(np.asarray([points]), axis=0))
-    pointcloud.normals = o3d.utility.Vector3dVector(np.squeeze(np.asarray([normals]), axis=0))
+    pointcloud.points = o3d.utility.Vector3dVector(np.squeeze(np.asarray([points]), axis=0))    
     pointcloud.colors = o3d.utility.Vector3dVector(np.squeeze(np.asarray([colors]), axis=0))
             
     return pointcloud
@@ -442,17 +342,14 @@ def remove_plane(pointcloud, threshold=0.005, init_n=3, iterations=1000):
 def poisson_disk_resampling(pointcloud, number_of_samples, radius=None):
     
     pc = pointcloud
-    points = np.squeeze(np.asarray([pc.points]), axis=0)
-    normals = np.squeeze(np.asarray([pc.normals]), axis=0)
+    points = np.squeeze(np.asarray([pc.points]), axis=0)    
     colors = np.squeeze(np.asarray([pc.colors]), axis=0)
     idx = pcu.downsample_point_cloud_poisson_disk(points, num_samples=number_of_samples)
-    pc.points = o3d.utility.Vector3dVector(points[idx])
-    pc.normals = o3d.utility.Vector3dVector(normals[idx])
+    pc.points = o3d.utility.Vector3dVector(points[idx])    
     pc.colors = o3d.utility.Vector3dVector(colors[idx])    
 
     #cl, idx = pc.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)        
-    pc.points = o3d.utility.Vector3dVector(points[idx])
-    pc.normals = o3d.utility.Vector3dVector(normals[idx])
+    pc.points = o3d.utility.Vector3dVector(points[idx])    
     pc.colors = o3d.utility.Vector3dVector(colors[idx])    
 
     return pc
